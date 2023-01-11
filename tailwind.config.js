3c856caf55ebc27907790b399e1d938ea59fc3e4
Dhendi Syafa <dhendisyafa@gmail.com>
2023-01-11 15:51:52 +0700

    feat: add configuration tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./component/**/*.{js, jsx, ts, tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'accent': '#D61C4E',
+        'grey': '#8e8e8e',
+      },
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
+}
