68972565c19affda1ea8973c73fe7b2586d132fa
Dhendi Syafa <dhendisyafa@gmail.com>
2023-01-13 14:01:48 +0700

    refactor: add mapping navbarIcon props in category

--- src/components/CategoryComponent.jsx
+++ src/components/CategoryComponent.jsx
@@ -3,37 +3,42 @@ import NavbarIcon from './navbar/NavbarIcon'
 import OpenBook from '../assets/photos/icon/book-open.svg'
 
 export default class CategoryComponent extends Component {
+
+  navLink = [
+    {
+      iconNav: OpenBook,
+      navbarName: "Bebas Baca",
+      weight: "bold"
+      // to: "/"
+    },
+    {
+      iconNav: OpenBook,
+      navbarName: "Novel",
+      weight: "bold"
+      // to: "/"
+    },
+    {
+      iconNav: OpenBook,
+      navbarName: "Komik",
+      weight: "bold"
+      // to: "/"
+    },
+    {
+      iconNav: OpenBook,
+      navbarName: "Majalah",
+      weight: "bold"
+      // to: "/"
+    }
+  ]
+
   render() {
     return (
       <div className="grid grid-cols-[auto_auto_auto_auto]">
-        {/* <NavbarIcon 
-            iconNav={OpenBook}
-            NavbarName="Bebas Baca" 
-            altIcon="Bebas Baca"
-            className={'font-bold'}/> */}
-        <NavbarIcon 
-            iconNav={OpenBook}
-            NavbarName="Bebas Baca" 
-            altIcon="Bebas Baca"
-            weight="bold"
-            />
-        <NavbarIcon 
-            iconNav={OpenBook}
-            NavbarName="Novel" 
-            altIcon="Novel"
-            weight="bold"
-            />
-        <NavbarIcon 
-            iconNav={OpenBook}
-            NavbarName="Komik" 
-            altIcon="Komik"
-            weight="bold"
-            />
-        <NavbarIcon 
-            iconNav={OpenBook}
-            NavbarName="Majalah" 
-            altIcon="Majalah"
-            weight="bold"/>
+        {this.navLink.map(({to, iconNav, navbarName, weight}, index) => {
+          return (
+            <NavbarIcon key={index} to={to} navbarName={navbarName} altIcon={navbarName} iconNav={iconNav} weight={weight}/>
+          )
+        })}
       </div>
     )
   }
