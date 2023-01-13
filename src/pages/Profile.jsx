9e2ac66b07c1140cc307bb1b7e517ca71c6d2a3a
Dhendi Syafa <dhendisyafa@gmail.com>
2023-01-13 16:01:32 +0700

    feat: change name component, bag to profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import photoProfil from '../assets/photos/product/2.png'
 import editProfile from '../assets/photos/icon/settings.svg'
 import logout from '../assets/photos/icon/log-out.svg'
 import CardSetting from '../components/CardSetting'
-export default class Bag extends Component {
+export default class Profile extends Component {
     render() {
         return (
             <div className='h-full'>
