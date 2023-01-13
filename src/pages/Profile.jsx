041c233b1449df98dae5d081dc58c8699307459b
Dhendi Syafa <dhendisyafa@gmail.com>
2023-01-13 13:55:19 +0700

    feat: change button back and delete logout button

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,22 +3,20 @@ import photoProfil from '../assets/photos/product/2.png'
 import editProfile from '../assets/photos/icon/settings.svg'
 import logout from '../assets/photos/icon/log-out.svg'
 import CardSetting from '../components/CardSetting'
-import classNames from 'classnames'
-
 export default class Bag extends Component {
     render() {
         return (
             <div className='h-full'>
                 <div className="flex justify-end mt-3 mr-3">
-                    <button className='bg-accent rounded-full text-white px-2 py-1 border-none'>X</button>
+                    <button className=' rounded-full text-accent px-2 py-0.5 hover:bg-accent hover:text-white ease-in duration-300'>X</button>
                 </div>
                 <div className="px-6 py-4 mt-4">
                     <div className="flex justify-between items-center border-b-2 pb-6">
                         <div className="flex items-center gap-3">
                             <img src={photoProfil} alt="photoprofil" className='h-14 w-14 rounded-full' />
                             <div className="">
-                                <p className='font-semibold text-sm'>Periplus Bandung</p>
-                                <p className='font-light text-xs'>dhensganteng@gmail.com</p>
+                                <p className='font-semibold text-sm leading-4'>Dhendi Syafa Athallah Putra</p>
+                                <p className='font-light text-xs mb-1'>dhensganteng@gmail.com</p>
                                 <p className='font-extralight text-xs'>089123456789</p>
                             </div>
                         </div>
@@ -40,10 +38,6 @@ export default class Bag extends Component {
                             nameSetting="Tema Aplikasi" />
                     </div>
 
-                </div>
-                <div className="relative flex justify-between items-end w-full h- py-4 px-4">
-                    <p className='font-sm text-normal'>Keluar</p>
-                    <img src={logout} alt="" />
                 </div>
             </div>
         )
